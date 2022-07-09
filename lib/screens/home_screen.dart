fbe432fe71bedecf8574eab0332b5dd8bed8b3fd
Lucas Kenji <ra1957066@gmail.com>
2022-07-09 01:28:27 -0300

    Habit Tracker: Implement list and edit screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:aegis_app/screens/habits_screen.dart';
 import 'package:aegis_app/screens/posts_screen.dart';
 import 'package:aegis_app/screens/todo_list_screen.dart';
 import 'package:flutter/material.dart';
+import '../models/habit.dart';
 import '../models/post.dart';
 import '../models/todo_card.dart';
 
@@ -17,6 +19,7 @@ class HomeScreenState extends State<HomeScreen>
 {
   var _posts = <Post>[];
   var _todoCards = <TodoCard>[];
+  var _habits = <Habit>[];
 
   void _addPost(Post newPost){
     setState(() => _posts.add(newPost));
@@ -24,6 +27,10 @@ class HomeScreenState extends State<HomeScreen>
 
   void _addTodoCard(TodoCard newTodoCard){
     setState(() => _todoCards.add(newTodoCard));
+  }
+
+  void _addHabit(Habit newHabit){
+    setState(() => _habits.add(newHabit));
   }
 
   @override
@@ -63,6 +70,16 @@ class HomeScreenState extends State<HomeScreen>
                     alignment: Alignment.centerLeft
                 ),
                 child: Text('To-do List'),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return HabitsScreen(_habits, _addHabit);
+                })),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(40),
+                    alignment: Alignment.centerLeft
+                ),
+                child: Text('Habit Tracker'),
               )
             ],
           )
